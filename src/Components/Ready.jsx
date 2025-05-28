17d6705971a9ef2a3882722006c1c46a7061fb51
Zubain-Nadeem <zubainali23@gmail.com>
2025-05-28 09:56:21 -0700

    Ready Section Created

--- src/Components/Ready.jsx
+++ src/Components/Ready.jsx
@@ -8,7 +8,7 @@ const Ready = () => {
                     <h2 className="text-[70px] font-bold">READY TO START</h2>
                     <p className="text-white text-[18px]">Our solar power experts will help you find the perfect solution for your home.</p>
                     <button className="hover:bg-[#fff] hover:text-black text-white cursor-pointer px-[26px] py-[10px] bg-[#EB7F25] font-normal border-none rounded-2xl text-[15px]">
-                        Calculate Solar
+                        Calculate SOlar
                     </button>
                 </div>
             </section>
